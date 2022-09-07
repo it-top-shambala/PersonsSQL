5f1edf3e074dd72937bba84616a03dfd910bc49e
Starinin Andrey <starininandrey@gmail.com>
2022-09-07 19:37:32 +0300

    Add HANDLER FOR SQLEXCEPTION

--- transaction_demo.sql
+++ transaction_demo.sql
@@ -11,3 +11,26 @@ BEGIN
 END |
 
 CALL add_('first_name', 'first_name', 'first_name');
+
+DELIMITER |
+CREATE PROCEDURE procedure_add_data_exception(IN last_name TEXT, IN first_name TEXT, IN patronymic_ TEXT)
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+        BEGIN
+            ROLLBACK;
+            SELECT 'ERROR!';
+        END;
+
+    START TRANSACTION;
+    CALL procedure_add_family(last_name);
+    CALL procedure_add_name(first_name);
+    CALL procedure_add_patronymic(patronymic_);
+
+    INSERT INTO table_persons(name_id, family_id, patronymic_id)
+        VALUE ((SELECT name_id FROM table_names WHERE name = first_name),
+               (SELECT family_id FROM table_families WHERE family = last_name),
+               (SELECT patronymic_id FROM table_patronymics WHERE patronymic = patronymic_));
+    COMMIT;
+END |
+
+CALL procedure_add_data_exception('Starinin', 'Andrey', NULL);
